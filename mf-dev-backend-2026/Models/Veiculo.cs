1cf04928005ca183649bbdafc951bdea48443ceb
Gabriela Alves Ignácio <alvesignaciog@gmail.com>
2026-04-26 21:56:41 +0100

    Criação da Classe Veiculo

---
+++ mf-dev-backend-2026/Models/Veiculo.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace mf_dev_backend_2026.Models
+{
+    [Table("Veiculos")]
+    public class Veiculo
+    {
+        [Key]
+        public int Id {get; set;}
+
+        [Required (ErrorMessage = "O nome do veículo é obrigatório.")]
+        public string Nome { get; set; }
+
+        [Required (ErrorMessage = "A marca do veículo é obrigatória.")]
+        public string Marca { get; set; }
+
+        [Required (ErrorMessage = "A placa do veículo é obrigatória.")]
+        public string Placa { get; set; }
+
+        [Required (ErrorMessage = "O ano de fabricação do veículo é obrigatório.")]
+        public int AnoFabricacao { get; set; }
+
+        [Required (ErrorMessage = "O ano do modelo do veículo é obrigatório.")]
+        public int AnoModelo { get; set; }
+
+
+
+    }
+}
