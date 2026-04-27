2b494bc0afadee50ab7da66240d069e681091cce
Gabriela Alves Ignácio <alvesignaciog@gmail.com>
2026-04-27 11:08:47 +0100

    Criação das funções do CRUD de veículos

--- mf-dev-backend-2026/Controllers/VeiculosController.cs
+++ mf-dev-backend-2026/Controllers/VeiculosController.cs
@@ -1,6 +1,7 @@
 ﻿using mf_dev_backend_2026.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using System.Runtime.InteropServices;
 
 namespace mf_dev_backend_2026.Controllers
 {
@@ -37,6 +38,76 @@ namespace mf_dev_backend_2026.Controllers
             return View(veiculo);
         }
 
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+                return NotFound();
+            
+            var dados = await _context.Veiculos.FindAsync(id);
+            
+            if (dados == null)
+                return NotFound();
+            
+            return View(dados);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(int Id, Veiculo dados)
+        {
+            if (Id != dados.Id)
+                return NotFound();
+
+
+            if (ModelState.IsValid)
+            {
+                _context.Veiculos.Update(dados);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        public async Task<IActionResult> Details(int? Id)
+        {
+            if (Id == null)
+                return NotFound();
+
+            var dados = await _context.Veiculos.FindAsync(Id);
+
+            if (dados == null)
+                return NotFound(); 
+
+            return View(dados);
+        }
+
+        public async Task<IActionResult> Delete(int? Id)
+        {
+            if (Id == null)
+                return NotFound();
+
+            var dados = await _context.Veiculos.FindAsync(Id);
+
+            if (dados == null)
+                return NotFound();
+
+            return View(dados);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public async Task<IActionResult> DeleteConfirmed(int Id)
+        {
+            if(Id == null)
+                return NotFound();
+
+            var dados = await _context.Veiculos.FindAsync(Id);
+            
+            if (dados == null)
+                return NotFound();
+
+            _context.Veiculos.Remove(dados);
+            await _context.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
 
     }
 }
